024ca97f20c30ecd79d311b243ac8968201f5556
Adrian Mason <adrianlmason@gmail.com>
2021-04-20 15:30:05 -0500

    Layout, Header, and Footer

---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout'
+
+export default function AddEventPage() {
+  return (
+    <Layout title='Add Event'>
+      <h1>Add an Event</h1>
+    </Layout>
+  )
+}
