73bd6b6ea0679a5fdd73a795b3bc7783247954f9
Adrian Mason <adrianlmason@gmail.com>
2021-05-18 15:08:55 -0500

    Switched to getServerSideProps in slug page

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>All we wanna do, is PARTY with YOU!</h1>
+      <h2>Find the hottest DJ events in your area!</h2>
+    </div>
+  )
+}
--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <Layout title='About DJ Events'>
       <h1>About</h1>
-      <p>This is an app to find the latest DJ and other musical events</p>
+      <p>This is an app to find the latest DJ party events as well as the ability to register and add events of your own!</p>
       <p>Version: 1.0.0</p>
     </Layout>
   )
